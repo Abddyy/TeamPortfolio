cf3b9b4bd9077f5a2edabb32b560984e33e55b43
OmarHaji0001 <s12029055@stu.najah.edu>
2025-03-24 21:53:29 +0200

    Creating models: project, teammember and skills with many to many field with project model creating structure for html: base components and pages and those inherits for index and from each other to ease the development process. Creating context_processor file for Global Data Access

--- TPortfolioApplication/admin.py
+++ TPortfolioApplication/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import *
 
 admin.site.register(Project)
+admin.site.register(TeamMember)
+admin.site.register(Skill)
 
--- pyrun.ps1
+++ pyrun.ps1
@@ -2,3 +2,5 @@ python manage.py makemigrations
 python manage.py migrate
 #python manage.py runserver 192.168.88.7:8000
 python manage.py runserver 127.0.0.1:5000
+
+#set-alias -Name pyr -Value .\pyrun.ps1  then-> you can only type pyr
